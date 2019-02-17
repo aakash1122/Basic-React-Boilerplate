e82f8d4f13a65103157572dbd672283f0e317858
aakash1122 <aakaashhossain@gmail.com>
2019-02-17 20:03:38 +0600

    Basic React Boilerplate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* make change here and see magic */}
+      </div>
+    );
+  }
+}
+
+export default App;
